2ea8c17c7e5a4e4a2c8dce7dc1212aa1ab6b2f41
Davi Santos de Araujo Boni <bonidavi07@gmail.com>
2024-11-19 12:27:32 -0300

    Concluindo exerc. bibli_05

---
+++ 03_bibliotecas/bibli_05/Resultados/Davi/completo/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "string_utils.h"
+
+int main() {
+    char string[100];
+    char stringCopiada[100];
+    scanf("%[^\n]", string);
+    int numeroSelecionado = 0;
+    while(numeroSelecionado != 6){
+        printf("1 - Tamanho da string\n2 - Copiar string\n3 - Converter string para letras maiusculas\n4 - Converter string para letras minusculas\n5 - Imprimir string ao contrario\n6 - Encerrar o programa\nOpcao escolhida: \n");
+    
+
+    scanf("%d", &numeroSelecionado);
+
+    if(numeroSelecionado == 1){
+        printf("Tamanho da string: %d\n", string_length(string));
+    }
+
+    if(numeroSelecionado == 2){
+        string_copy(string, stringCopiada);
+        printf("String copiada: %s\n", stringCopiada );
+    }
+
+    if(numeroSelecionado == 3){
+        string_upper(string);
+        printf("String convertida para maiusculas: %s", string);
+    }
+
+    if(numeroSelecionado == 4){
+        string_lower(string);
+        printf("String convertida para minusculas: %s", string);
+    }
+
+    if(numeroSelecionado == 5){
+        string_reverse(string);
+        printf("String invertida: %s", string);
+    }
+
+    }
+
+
+}
